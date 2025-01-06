2f2da931e138a573dabfbd17898adf039cacd2f7
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-01-06 13:47:35 +0530

    add logo and footer for subjects

---
+++ studentCare/components/Subjects.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { Image, ScrollView, StyleSheet, View } from "react-native";
+import {
+  Button,
+  Divider,
+  PaperProvider,
+  Text,
+  TextInput,
+  List,
+} from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
+
+export default function Subjects() {
+
+  return (
+    <PaperProvider>
+      <ScrollView style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>UoV Student Care</Text>
+        </View>
+        <View style={styles.imagecontain}>
+          <Image
+            source={require("../assets/uovlogo.png")}
+            style={styles.image}
+          />
+        </View>
+        <View style={styles.headerSubject}>
+          <Text style={styles.headerTextsubject}>Computer Science</Text>
+        </View>
+
+        <View style={styles.header}>
+          <Text style={styles.headerText}>UoV @ 2025</Text>
+        </View>
+      </ScrollView>
+    </PaperProvider>
+  );
+}
