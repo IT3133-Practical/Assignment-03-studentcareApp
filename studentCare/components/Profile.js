0ebe7b0f852e60811e4ef60d23324899eb431748
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-01-06 13:41:08 +0530

    add more fields for profile

--- studentCare/components/Profile.js
+++ studentCare/components/Profile.js
@@ -23,6 +23,37 @@ export default function Profile({ route }) {
             style={styles.image}
           />
         </View>
+
+        <View style={styles.studentInfoContainer}>
+          <Image
+            style={styles.profilePicContainer}
+            source={studentdata.profile_pic}
+          ></Image>
+          <Text style={styles.studentName}>Name:{studentdata.username}</Text>
+          <Text style={styles.studentDetails}>Age:{studentdata.age}</Text>
+          <Text style={styles.studentDetails}>Gender:{studentdata.gender}</Text>
+        </View>
+
+        <View style={styles.contactInfoContainer}>
+          <Text style={styles.contactTitle}>Contact Information</Text>
+          <Text style={styles.contactDetails}>Email: {studentdata.email}</Text>
+          <Text style={styles.contactDetails}>Phone: {studentdata.phone}</Text>
+          <Text style={styles.contactDetails}>
+            Address: {studentdata.address}
+          </Text>
+        </View>
+
+        <Divider></Divider>
+        <View style={styles.biologicalInfoContainer}>
+          <Text style={styles.biologicalTitle}>Biological Information</Text>
+          <Text style={styles.biologicalDetails}>
+            Gender: {studentdata.gender}
+          </Text>
+          <Text style={styles.biologicalDetails}>Age: {studentdata.age}</Text>
+          <Text style={styles.biologicalDetails}>
+            Blood Group: {studentdata.blood_group}
+          </Text>
+        </View>
       </ScrollView>
     </PaperProvider>
   );
