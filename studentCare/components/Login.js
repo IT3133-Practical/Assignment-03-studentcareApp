0e01d34940e41409c599ec98ec3251030c3c3381
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-01-06 13:32:17 +0530

    add text input field

--- studentCare/components/Login.js
+++ studentCare/components/Login.js
@@ -6,6 +6,9 @@ import {
   Text,
   TextInput,
 } from "react-native-paper";
+import { students } from "../data/StudentsDb";
+import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Login() {
 
@@ -21,6 +24,25 @@ export default function Login() {
             source={require("../assets/uovlogo.png")}
             style={styles.image}
           />
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.loginText}>Student Login</Text>
+          <Divider />
+        </View>
+
+        <View style={styles.formcontainer}>
+          <TextInput label="Username" mode="outlined" onChangeText={setName} />
+          <TextInput
+            label="Password"
+            mode="outlined"
+            secureTextEntry
+            onChangeText={setPassword}
+          />
+          <View style={styles.buttoncontainer}>
+            <Button mode="contained" onPress={handlebutton}>
+              Login
+            </Button>
+          </View>
         </View>
       </ScrollView>
     </PaperProvider>
